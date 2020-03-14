80e7039e48977874e9b9305d0581c6f89ba99300
tatusko555 <tatusko555@gmail.com>
2020-03-13 22:31:28 -0400

    one more class added

--- src/Test1.java
+++ src/Test1.java
@@ -3,7 +3,9 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello");
+		System.out.println("second change");
+		
 	} 
 
 }
